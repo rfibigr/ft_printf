0d82770e2f9270f2ef3a0bc0005c9f8dc9d0c3df
Romain FIBIGR <rfibigr@e2r5p3.42.fr>
2018-05-30 19:05:22 +0200

    printf

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,12 +6,17 @@
 /*   By: rfibigr <rfibigr@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/30 12:32:04 by rfibigr           #+#    #+#             */
-/*   Updated: 2018/05/30 15:50:06 by rfibigr          ###   ########.fr       */
+/*   Updated: 2018/05/30 18:35:11 by rfibigr          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "printf.h"
 
-int	ft_printf(const char **str, ...)
+int	ft_printf(const char *str, ...)
 {
+	va_list		ap;
 
+	va_start(ap, str);
+	type va_arg(ap, type);
+	va_end(ap);
 }
