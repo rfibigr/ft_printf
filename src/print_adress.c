8a63d61a5d0919d772932eadc0a205eb888712ae
vagrant <vagrant@vagrant.local>
2018-08-13 20:41:54 +0000

    adress

--- src/print_adress.c
+++ src/print_adress.c
@@ -6,7 +6,7 @@
 /*   By: rfibigr <rfibigr@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/13 11:36:21 by rfibigr           #+#    #+#             */
-/*   Updated: 2018/08/13 18:05:05 by rfibigr          ###   ########.fr       */
+/*   Updated: 2018/08/13 21:42:32 by rfibigr          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,29 @@
 
 void	print_adress(va_list ap, t_param param, t_buff *buff)
 {
+	int size;
 	unsigned long long int arg;
-	t_padding padding;
 
-	padding.sign = 0;
-	param.conver = 'x';
-	param.flag[e_flag_hastag] = TRUE;
 	arg = (unsigned long long int)va_arg(ap, unsigned long long int);
-	padding.size = size_adress(arg);
-	if (padding.precision == -1)
-		padding.width--;
-	padding_struct(&padding, param);
-	padding_before(param, padding, buff);
-	if (!(arg == 0 && param.precision != -1))
-		print_unsigned_l((unsigned long long int)arg, &param, buff);
-	if (param.flag[e_flag_less])
-		ft_print_charact(padding.width, buff, ' ');
-}
-
-int		size_adress(unsigned long adress)
-{
-	int i;
-
-	i = 0;
-	while (adress != 0)
+	size = size_lu_nbr(arg, param);
+	param.precision -= size;
+	if (param.precision > 0)
+		param.width = param.width - param.precision - size - 2;
+	else
+		param.width = param.width - size - 2;
+	if (param.width > 0 && param.flag[e_flag_less] == 0 && param.flag[e_flag_zero] == 0)
+		ft_print_charact(param.width, buff, ' ');
+	ft_print_str(buff, "0x");
+	if (param.width > 0 && param.flag[e_flag_less] == 0 && param.flag[e_flag_zero] == 1)
+		ft_print_charact(param.width, buff, '0');
+	if (param.precision > 0)
+		ft_print_charact(param.precision, buff, '0');
+	// si precision nombre de 0
+	if (size != 0)
 	{
-		i++;
-		adress /= 16;
+		param.conver = 'x';
+		print_unsigned_l(arg, &param, buff);
 	}
-	return (i);
+	if (param.width > 0 && param.flag[e_flag_less] == 1)
+		ft_print_charact(param.width, buff, ' ');
 }
